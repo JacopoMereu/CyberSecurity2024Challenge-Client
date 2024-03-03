6d936466989b275a992ac2aff15e49cf35e0cf6d
JacopoMereu <jachi334@gmail.com>
2024-03-03 11:42:12 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,5 @@
+function test() {
+  alert("Test JavaScript file loaded successfully!)  
+  console.log("Test JavaScript file loaded successfully!");
+  prinnt("Test JavaScript file loaded successfully!")
+}
